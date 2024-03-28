73c8d0f7ec48e5aaa03fa658e696dc2ccc39ad55
GGNOOB1 <geraldovicente987@gmail.com>
2024-03-28 19:06:11 -0300

    Add: Criando navegação tab e drawer para o app

---
+++ src/navigation/DrawerNavigator.tsx
@@ -0,0 +1,20 @@
+/*import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigator from './TabNavigator.js';
+import ConfigScreen from '../screens/menu/configScreen.js';
+import CustomDrawerNavigatorProfessor from '../components/menu-professor.js';
+
+
+const Drawer = createDrawerNavigator();
+
+
+export default function DrawerNavigator() {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator screenOptions drawerContent={props => <CustomDrawerNavigatorProfessor {...props} />}>
+                <Drawer.Screen name="Nome" component={TabNavigator} />
+                <Drawer.Screen name="Config" component={ConfigScreen} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+} */
---
+++ src/navigation/TabNavigator.tsx
@@ -0,0 +1,86 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import React from 'react';
+import Home from '../screens/Home/index';
+import Calendario from '../screens/Calendario/index';
+import Mapa from '../screens/Mapa/index';
+import Salas from '../screens/Salas/index';
+import Notificacoes from '../screens/Notificacoes/index';
+import { NavigationContainer } from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+const Tab = createBottomTabNavigator();
+
+
+export default function TabNavigator() {
+    return (
+
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={
+                    {
+                        headerShown: false,
+                        tabBarStyle: {
+                            backgroundColor: '#fff',
+                            minHeight: '9%',
+                            maxHeight: '9%',
+                        },
+
+                        tabBarLabelStyle: {
+                            position: 'relative',
+                            marginBottom: '17%'
+                        }
+
+                    }}>
+                <Tab.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                        tabBarIcon: ({ color, size }) => <Ionicons
+                            name='home-outline'
+                            color={color} size={size}
+                            style={{ marginTop: '15%' }} />,
+                    }}
+                />
+                <Tab.Screen
+                    name="Calendário"
+                    component={Calendario}
+                    options={{
+                        tabBarIcon: ({ color, size }) => <Ionicons
+                            name='calendar-outline' color={color} size={size}
+                            style={{ marginTop: '15%' }} />
+                    }}
+                />
+                <Tab.Screen
+                    name="Mapa"
+                    component={Mapa}
+                    options={{
+                        tabBarIcon: ({ color, size }) => <Ionicons
+                            name='map-outline' color={color} size={size}
+                            style={{ marginTop: '15%' }} />
+                    }}
+                />
+                <Tab.Screen
+                    name="Salas"
+                    component={Salas}
+                    options={{
+                        tabBarIcon: ({ color, size }) => <Ionicons
+                            name='list-outline' color={color} size={size}
+                            style={{ marginTop: '15%' }} />
+                    }}
+                />
+                <Tab.Screen
+                    name="Notificações"
+                    component={Notificacoes}
+                    options={{
+                        tabBarIcon: ({ color, size }) => <Ionicons
+                            name='notifications-outline' color={color} size={size}
+                            style={{ marginTop: '15%' }} />
+                    }}
+                />
+
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
