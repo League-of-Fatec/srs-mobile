f5944529d2fcafc2faed65cd3ac517a485c39b18
GGNOOB1 <geraldovicente987@gmail.com>
2024-03-28 19:01:55 -0300

    Add: Estrutura e style da tela Notificações

---
+++ src/screens/Notificacoes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles';
+
+export default function Notificacoes() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Notificacoes</Text>
+        </View>
+    );
+}
+
