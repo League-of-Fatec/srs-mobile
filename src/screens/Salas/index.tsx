a717174c6061ded0ec904e2b1b14b8f766dac8e3
GGNOOB1 <geraldovicente987@gmail.com>
2024-03-28 19:01:23 -0300

    Add: Estrutura e style da tela Salas

---
+++ src/screens/Salas/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './styles';
+
+export default function Salas() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Tela Salas</Text>
+        </View>
+    );
+}
+
