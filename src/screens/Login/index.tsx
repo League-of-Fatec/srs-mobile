7f2898e0b85917f0472db1e033a67a83787ee3d6
GGNOOB1 <geraldovicente987@gmail.com>
2024-03-28 21:03:18 -0300

    Fix: Consertando estrutura e estilo de tela de login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,12 +1,61 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, Button, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 import styles from './styles';
+import { TextInput } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Login() {
+
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [isSelected, setSelection] = useState(false);
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Screen1</Text>
-        </View>
+        <View style={styles.containerFoto}>
+            <View style={{ flex: 1, height: '30%', width: '100%' }}>
+                <Image source={require("@/assets/images/img-reuniao.jpg")} style={styles.image}></Image>
+
+                <View style={styles.container}>
+
+
+                    <GestureHandlerRootView style={{ flex: 1 }}>
+                        <Text style={styles.textEmail}>E-mail</Text>
+                        <View style={styles.inputView}>
+                            <TextInput style={styles.TextInput}
+                                placeholder='Digite seu email.'
+                                placeholderTextColor={'#003f5c'}
+                                onChangeText={(email) => setEmail(email)} />
+                        </View>
+
+                        <Text style={styles.textSenha}>Senha</Text>
+                        <View style={styles.inputView}>
+                            <TextInput
+                                style={styles.TextInput}
+                                placeholder="Digite sua Senha."
+                                placeholderTextColor="#003f5c"
+                                secureTextEntry={true}
+                                onChangeText={(senha) => setSenha(senha)} />
+                        </View>
+
+                        <View>
+                            <Text style={{}}>Manter-se Conectado</Text>
+                        </View>
+
+
+                        <TouchableOpacity style={styles.loginBtn}>
+                            <Text style={{}}>ENTRAR</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity>
+                            <Text style={styles.forgot_button}>Esqueci minha senha</Text>
+                        </TouchableOpacity>
+                    </GestureHandlerRootView>
+                </View>
+
+            </View>
+        </View >
     );
 }
-
--- src/screens/Login/styles.tsx
+++ src/screens/Login/styles.tsx
@@ -1,13 +1,58 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
+    containerFoto: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
-    text: {
-        fontSize: 20,
-        fontWeight: 'bold',
+    container: {
+        flex: 1,
+        paddingLeft: '10%',
+        paddingRight: '10%'
+
     },
+    textEmail: {
+        fontSize: 20,
+        marginTop: 63,
+    },
+    textSenha: {
+        fontSize: 20,
+        marginTop: 10,
+    },
+    image: {
+        height: '30%',
+        width: '100%',
+    },
+    inputView: {
+        backgroundColor: "#E5E4E2",
+        borderRadius: 20,
+        width: '100%',
+        height: 45,
+        marginTop: 2,
+        marginBottom: 20,
+        alignItems: "flex-start",
+    },
+    TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+    },
+    forgot_button: {
+        height: 30,
+        marginTop: '10%',
+        color: '#3E74FF',
+        marginLeft: '27%',
+        marginRight: '27%',
+    },
+    loginBtn: {
+        width: "100%",
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 20,
+        backgroundColor: "#3E74FF",
+    },
+
 });
