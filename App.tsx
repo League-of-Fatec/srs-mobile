642f37b6a8b9dc1a97a8aa87268c1d6a6a5b5f02
GGNOOB1 <geraldovicente987@gmail.com>
2024-03-28 21:03:59 -0300

    Add: Estrutura teste de login

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import TabNavigator from './src/navigation/TabNavigator';
+import Login from '@/screens/Login';
 
 export default function App() {
-  return <TabNavigator />;
+
+  const logado = false;
+
+  if (logado) {
+    return <TabNavigator />;
+  } if (!logado) {
+    return Login()
+  } else {
+    return alert("Erro no login!")
+  }
+
 }
