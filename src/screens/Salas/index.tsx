091796e26efb9e82c985dff84a664d4aed4ddea7
GGNOOB1 <geraldovicente987@gmail.com>
2024-04-15 18:59:52 -0300

    Fix: Modificando a tela de Salas e a estilizando

--- src/screens/Salas/index.tsx
+++ src/screens/Salas/index.tsx
@@ -1,11 +1,106 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, ScrollView } from 'react-native';
 import styles from './styles';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 export default function Salas() {
+
+    const [corBotao, setarCorBotao] = useState(['#6DCE31', '#FAAF40', '#B54646']);
+
     return (
+
         <View style={styles.container}>
-            <Text style={styles.text}>Tela Salas</Text>
+            <View style={styles.header}>
+                <Text style={styles.headerTitulo}>Salas</Text>
+            </View>
+            <View style={styles.containerAndares}>
+                <Text style={styles.nomeAndar}>1° Andar</Text>
+                <View style={styles.containerSalas}>
+
+                    <ScrollView>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[0] }]}>
+                            <Text style={styles.nomeSala}>Sala 10</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[1] }]}>
+                            <Text style={styles.nomeSala}>Sala 11</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[2] }]}>
+                            <Text style={styles.nomeSala}>Sala 12</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[0] }]}>
+                            <Text style={styles.nomeSala}>Sala 13</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[0] }]}>
+                            <Text style={styles.nomeSala}>Sala 8</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[2] }]}>
+                            <Text style={styles.nomeSala}>Sala 9</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                    </ScrollView>
+
+                </View>
+
+                <Text style={styles.nomeAndar}>2° Andar</Text>
+                <View style={styles.containerSalas}>
+                    <ScrollView>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[0] }]}>
+                            <Text style={styles.nomeSala}>Sala 20</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[1] }]}>
+                            <Text style={styles.nomeSala}>Sala 21</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[2] }]}>
+                            <Text style={styles.nomeSala}>Sala 22</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[0] }]}>
+                            <Text style={styles.nomeSala}>Sala 26</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[0] }]}>
+                            <Text style={styles.nomeSala}>Sala 27</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={[styles.botaoSala, { backgroundColor: corBotao[2] }]}>
+                            <Text style={styles.nomeSala}>Sala 28</Text>
+                            <Ionicons
+                                style={styles.infoIcons} name='desktop-outline'
+                            />
+                        </TouchableOpacity>
+                    </ScrollView>
+                </View>
+            </View>
         </View>
     );
 }
--- src/screens/Salas/styles.tsx
+++ src/screens/Salas/styles.tsx
@@ -3,11 +3,58 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     container: {
         flex: 1,
+    },
+    header: {
+        backgroundColor: '#D9D9D9',
+        flexDirection: 'row',
+        width: '100%',
+        height: 87,
         justifyContent: 'center',
         alignItems: 'center',
+        borderBottomLeftRadius: 35,
+        borderBottomRightRadius: 35
+    },
+    headerTitulo: {
+        fontSize: 20,
+        fontWeight: 'bold',
     },
     text: {
         fontSize: 20,
         fontWeight: 'bold',
+    },
+    containerAndares: {
+        flex: 1,
+        marginTop: '8%',
+        marginHorizontal: '3%',
+        //backgroundColor: 'pink',
+    },
+    containerSalas: {
+        flex: 1,
+        margin: '4%',
+        //backgroundColor: 'blue',
+    },
+    nomeAndar: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    botaoSala: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        //backgroundColor: 'yellow',
+        height: 57,
+        borderRadius: 7,
+        marginVertical: 5
+    },
+    nomeSala: {
+        flex: 1,
+        marginLeft: 13,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    infoIcons: {
+        fontSize: 25,
+        marginRight: 12,
+
     },
 });
