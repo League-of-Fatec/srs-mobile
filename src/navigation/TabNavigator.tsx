da694ba35ad0d2f3a6ae68edcfb39ef694c067a8
GGNOOB1 <geraldovicente987@gmail.com>
2024-04-15 19:01:18 -0300

    Fix: Removendo NavigationContainer para evitar erro

--- src/navigation/TabNavigator.tsx
+++ src/navigation/TabNavigator.tsx
@@ -16,72 +16,70 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
     return (
+        <Tab.Navigator
+            screenOptions={
+                {
+                    headerShown: false,
+                    tabBarStyle: {
+                        backgroundColor: '#fff',
+                        minHeight: '9%',
+                        maxHeight: '9%',
+                    },
 
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={
-                    {
-                        headerShown: false,
-                        tabBarStyle: {
-                            backgroundColor: '#fff',
-                            minHeight: '9%',
-                            maxHeight: '9%',
-                        },
+                    tabBarLabelStyle: {
+                        position: 'relative',
+                        marginBottom: '17%'
+                    }
 
-                        tabBarLabelStyle: {
-                            position: 'relative',
-                            marginBottom: '17%'
-                        }
+                }}>
+            <Tab.Screen
+                name="Home"
+                component={HomeStackNavigator}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Ionicons
+                        name='home-outline'
+                        color={color} size={size}
+                        style={{ marginTop: '15%' }} />,
+                }}
+            />
+            <Tab.Screen
+                name="Calendário"
+                component={Calendario}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Ionicons
+                        name='calendar-outline' color={color} size={size}
+                        style={{ marginTop: '15%' }} />
+                }}
+            />
+            <Tab.Screen
+                name="Mapa"
+                component={Mapa}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Ionicons
+                        name='map-outline' color={color} size={size}
+                        style={{ marginTop: '15%' }} />
+                }}
+            />
+            <Tab.Screen
+                name="Salas"
+                component={Salas}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Ionicons
+                        name='list-outline' color={color} size={size}
+                        style={{ marginTop: '15%' }} />
+                }}
+            />
+            <Tab.Screen
+                name="Notificações"
+                component={Notificacoes}
+                options={{
+                    tabBarIcon: ({ color, size }) => <Ionicons
+                        name='notifications-outline' color={color} size={size}
+                        style={{ marginTop: '15%' }} />
+                }}
+            />
 
-                    }}>
-                <Tab.Screen
-                    name="Home"
-                    component={HomeStackNavigator}
-                    options={{
-                        tabBarIcon: ({ color, size }) => <Ionicons
-                            name='home-outline'
-                            color={color} size={size}
-                            style={{ marginTop: '15%' }} />,
-                    }}
-                />
-                <Tab.Screen
-                    name="Calendário"
-                    component={Calendario}
-                    options={{
-                        tabBarIcon: ({ color, size }) => <Ionicons
-                            name='calendar-outline' color={color} size={size}
-                            style={{ marginTop: '15%' }} />
-                    }}
-                />
-                <Tab.Screen
-                    name="Mapa"
-                    component={Mapa}
-                    options={{
-                        tabBarIcon: ({ color, size }) => <Ionicons
-                            name='map-outline' color={color} size={size}
-                            style={{ marginTop: '15%' }} />
-                    }}
-                />
-                <Tab.Screen
-                    name="Salas"
-                    component={Salas}
-                    options={{
-                        tabBarIcon: ({ color, size }) => <Ionicons
-                            name='list-outline' color={color} size={size}
-                            style={{ marginTop: '15%' }} />
-                    }}
-                />
-                <Tab.Screen
-                    name="Notificações"
-                    component={Notificacoes}
-                    options={{
-                        tabBarIcon: ({ color, size }) => <Ionicons
-                            name='notifications-outline' color={color} size={size}
-                            style={{ marginTop: '15%' }} />
-                    }}
-                />
+        </Tab.Navigator>
 
-            </Tab.Navigator>
-        </NavigationContainer>
     );
 }
