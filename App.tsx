b09dd25e9ef7abe8a035eb23fbef543bd692ce4a
GGNOOB1 <geraldovicente987@gmail.com>
2024-04-15 19:01:57 -0300

    Fix: Modificando processo inicial do app

--- App.tsx
+++ App.tsx
@@ -2,17 +2,20 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import TabNavigator from './src/navigation/TabNavigator';
 import Login from '@/screens/Login';
+import AppNavigator from '@/navigation/AppNavigator';
 
 export default function App() {
 
-  const logado = true;
+  // const logado = true;
 
-  if (logado) {
-    return <TabNavigator />;
-  } if (!logado) {
-    return Login()
-  } else {
-    return alert("Erro no login!")
-  }
+  // if (logado) {
+  //   return <TabNavigator />;
+  // } if (!logado) {
+  //   return Login()
+  // } else {
+  //   return alert("Erro no login!")
+  // }
+
+  return <AppNavigator />
 
 }
