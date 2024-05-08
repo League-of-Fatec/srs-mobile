e17f3bb10f38cae25481aba79ffc00e1b70f5e9f
GGNOOB1 <geraldovicente987@gmail.com>
2024-05-08 16:12:34 -0300

    Fix: Removendo imports desnecessários

--- src/modules/aluno/screens/Home/index.tsx
+++ src/modules/aluno/screens/Home/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Image, Text, TouchableOpacity, View, ScrollView, Dimensions } from 'react-native';
+import { View, } from 'react-native';
 import styles from './styles';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { StatusBar } from 'expo-status-bar';
-import { List } from 'react-native-paper';
-
 import { NavigationProp } from '@react-navigation/native';
 import HeaderHome from './components/HeaderHome';
 import ProximasAulas from './components/ProximasAulas';
-import SalasFavoritas from './components/SalasFavoritas';
+import SalasFavoritas from "./components/SalasFavoritas";
 
 
 export default function Home({ navigation }: { navigation: NavigationProp<any> }) {
