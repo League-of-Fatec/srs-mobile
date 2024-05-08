61fd859d9accfaf0579e570f982d4c8e12174a6b
GGNOOB1 <geraldovicente987@gmail.com>
2024-05-08 16:13:13 -0300

    Add: Adicionando configuração da lingua portuguesa para o calendário

--- src/modules/aluno/screens/Calendario/utils/formatDate.ts
+++ src/modules/aluno/screens/Calendario/utils/formatDate.ts
@@ -1,34 +1,47 @@
-import { DateData } from "react-native-calendars";
+import { DateData } from 'react-native-calendars';
 
 const formatDate = (date: DateData, currentDay: number) => {
+  const months = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ];
+  const { day, month, year } = date;
 
-    const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
-    const { day, month, year } = date;
+  enum relativeDates {
+    Ontem = 'Ontem,',
+    Hoje = 'Hoje,',
+    Amanha = 'Amanhã,',
+  }
 
-    enum relativeDates {
-        Ontem = "Ontem,",
-        Hoje = "Hoje,",
-        Amanha = "Amanhã,",
-    }
+  let relativeDate = '';
 
-    let relativeDate = "";
+  if (day == currentDay) {
+    relativeDate = relativeDates.Hoje;
+  } else if (day == currentDay - 1) {
+    relativeDate = relativeDates.Ontem;
+  } else if (day == currentDay + 1) {
+    relativeDate = relativeDates.Amanha;
+  } else {
+    relativeDate = 'No';
+  }
 
-    if (day == currentDay) {
-        relativeDate = relativeDates.Hoje
-    } else if (day == currentDay - 1) {
-        relativeDate = relativeDates.Ontem
-    } else if (day == currentDay + 1) {
-        relativeDate = relativeDates.Amanha
-    } else {
-        relativeDate = "No"
-    }
-
-    return `${relativeDate} dia ${date.day} de ${months[date.month]} de ${date.year}
+  return `${relativeDate} dia ${date.day} de ${months[date.month]} de ${
+    date.year
+  }
     \nAs aulas serão as seguintes: \n
     Álgebra Linear\n
     Desenvolvimento Web III\n
     `;
-
-}
+};
 
 export default formatDate;
--- src/utils/ConfigCalendar.ts
+++ src/utils/ConfigCalendar.ts
@@ -1,49 +1,49 @@
-import { LocaleConfig } from "react-native-calendars";
+import { LocaleConfig } from 'react-native-calendars';
 
 const localeConfigData = LocaleConfig;
 
-localeConfigData.locales["br"] = {
+localeConfigData.locales['br'] = {
   monthNames: [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abrii",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abrii',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
   ],
   monthNamesShort: [
-    "Jan.",
-    "Fev",
-    "Mar",
-    "Abr",
-    "Mai",
-    "Jun",
-    "Jul",
-    "Ago",
-    "Set",
-    "Out",
-    "Nov",
-    "Dez",
+    'Jan.',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez',
   ],
   dayNames: [
-    "Domingo",
-    "Segunda",
-    "Terça",
-    "Quarta",
-    "Quinta",
-    "Sexta",
-    "Sábado",
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado',
   ],
-  dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui.", "Sex", "Sab"],
-  today: "Hoje",
+  dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui.', 'Sex', 'Sab'],
+  today: 'Hoje',
 };
 
-localeConfigData.defaultLocale = "br";
+localeConfigData.defaultLocale = 'br';
 
 export default localeConfigData;
