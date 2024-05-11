614b7bc69f53091751da04f4f85557aebe0d358a
GGNOOB1 <geraldovicente987@gmail.com>
2024-05-10 22:02:25 -0300

    Add: Adicionando interface com dados do User

---
+++ src/utils/User.ts
@@ -0,0 +1,15 @@
+export default interface User {
+  firstName: string;
+  lastName: string;
+  course: string;
+  semester: string;
+  registration: string;
+}
+
+const user = {
+  firstName: 'Geraldo',
+  lastName: 'Vicente',
+  course: 'DSM',
+  semester: '5°',
+  registration: '214921414',
+};
