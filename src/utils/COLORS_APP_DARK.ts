953c7657b8ef75b999c5bb4eb6bc51123a56f52b
GGNOOB1 <geraldovicente987@gmail.com>
2024-05-28 17:12:37 -0300

    styles: Cores de temas claro e escuro para o app

---
+++ src/utils/COLORS_APP_DARK.ts
@@ -0,0 +1,15 @@
+export const COLORS = {
+  corPrincipal: '#6D1C1C',
+  corSecundaria: '#D9D9D9',
+  corNeutra: '#FFFFFF',
+  tabBar: {
+    corPrincipal: '#6D1C1C',
+    corSecundaria: '#000000',
+  },
+  telaHome: {
+    carteirinha: {
+      corPrincipal: '#3A3A3A',
+      corSecundaria: '#D9D9D9',
+    },
+  },
+};
---
+++ src/utils/COLORS_APP_LIGHT.ts
@@ -0,0 +1,16 @@
+export const COLORS = {
+  corPrincipal: '#6D1C1C',
+  corSecundaria: '#D9D9D9',
+  corSecundaria2: '#F3F3F3',
+  corNeutra: '#FFFFFF',
+  tabBar: {
+    corPrincipal: '#6D1C1C',
+    corSecundaria: '#000000',
+  },
+  telaHome: {
+    carteirinha: {
+      corPrincipal: '#3A3A3A',
+      corSecundaria: '#D9D9D9',
+    },
+  },
+};
