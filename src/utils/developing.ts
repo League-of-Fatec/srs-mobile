e51b0516a33b2906c2c280a847800cfcf4e577aa
GGNOOB1 <geraldovicente987@gmail.com>
2024-05-28 17:12:56 -0300

    add: Mensagem de desenvolvimento para funcionalidades não implementadas

---
+++ src/utils/developing.ts
@@ -0,0 +1,8 @@
+import { Alert } from 'react-native';
+
+export const developing = () => {
+  Alert.alert(
+    'Em Desenvolvimento',
+    'Funcionalidade será implementada posteriormente!',
+  );
+};
