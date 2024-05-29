9daa140adaeee664a8b9bb437a5ab93c10e1b5be
GGNOOB1 <geraldovicente987@gmail.com>
2024-05-29 18:37:38 -0300

    Add: Adicionando loading a seção Próximas Aulas

--- src/modules/professor/screens/Home/components/ProximasAulas.tsx
+++ src/modules/professor/screens/Home/components/ProximasAulas.tsx
@@ -9,12 +9,15 @@ import { ProfessorState } from '@/redux/UserSlice';
 import { useSelector } from 'react-redux';
 import { ResponseTypeClassesByTeacherIdByWeekDay } from '../utils/ResponseTypeClassesByTeacherIdByWeekDay';
 import { formatTime } from '@/utils/formatTime';
+import LoadingSalasFavoritas from '@/components/shared/LoadingSalasFavoritas';
 
 const ProximasAulas = ({ navigation }: { navigation: NavigationProp<any> }) => {
 
     const professor = useSelector((state: { professor: ProfessorState }) => state.professor);
     const animatedRef = useAnimatedRef<ScrollView>();
     const [classes, setClasses] = useState<ResponseTypeClassesByTeacherIdByWeekDay>([]);
+
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
 
@@ -23,9 +26,19 @@ const ProximasAulas = ({ navigation }: { navigation: NavigationProp<any> }) => {
 
         (async () => {
             if (weekDay) {
-                const response = await fetch(`${api_url_local}/classes/${professor.professor?.id}/${weekDay}`)
-                const responseJson: ResponseTypeClassesByTeacherIdByWeekDay = await response.json();
-                setClasses(responseJson);
+                setLoading(true);
+
+                try {
+
+                    const response = await fetch(`${api_url_local}/classes/${professor.professor?.id}/${weekDay}`)
+                    const responseJson: ResponseTypeClassesByTeacherIdByWeekDay = await response.json();
+                    setClasses(responseJson);
+                } catch (error) {
+                    console.error(error);
+                } finally {
+                    setLoading(false);
+                }
+
             }
         })();
 
@@ -34,52 +47,58 @@ const ProximasAulas = ({ navigation }: { navigation: NavigationProp<any> }) => {
     return (
         <View style={styles.containerProximasAulas}>
             <Text style={styles.titleProximasAulas}>Próximas Aulas</Text>
-            <ScrollView
-                horizontal={true}
-                showsHorizontalScrollIndicator={true}
-                ref={animatedRef}
-                contentContainerStyle={{ paddingLeft: 10 }}
-            >
-                {classes.length === 0 ? (
-                    <View style={styles.viewNoClasses}>
-                        <Text style={styles.textNoClasses1}>Você não tem aulas no dia de hoje</Text>
-                        <Text style={styles.textNoClasses2}>Se quiser ver as aulas reservadas em qualquer dia no calendário</Text>
-                        <Text style={styles.textNoClasses3}>Clique no botão Ver Mais</Text>
-                    </View>
-                ) : (
-                    classes.map((currentClass, index) => {
-                        return (
-                            <TouchableOpacity style={styles.itemAula} key={index}>
-                                <View style={styles.cardAula}>
-                                    <Ionicons style={styles.iconProf}
-                                        name='person' />
-                                    <View style={styles.cardDescAula}>
-                                        {/* <Text style={styles.nomeProfessor}>
+            {isLoading ? (
+                <LoadingSalasFavoritas />
+            ) : <>
+                <ScrollView
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={true}
+                    ref={animatedRef}
+                    contentContainerStyle={{ paddingLeft: 10 }}
+                >
+                    {classes.length === 0 ? (
+                        <View style={styles.viewNoClasses}>
+                            <Text style={styles.textNoClasses1}>Você não tem aulas no dia de hoje</Text>
+                            <Text style={styles.textNoClasses2}>Se quiser ver as aulas reservadas em qualquer dia no calendário</Text>
+                            <Text style={styles.textNoClasses3}>Clique no botão Ver Mais</Text>
+                        </View>
+                    ) : (
+                        classes.map((currentClass, index) => {
+                            return (
+                                <TouchableOpacity style={styles.itemAula} key={index}>
+                                    <View style={styles.cardAula}>
+                                        <Ionicons style={styles.iconProf}
+                                            name='person' />
+                                        <View style={styles.cardDescAula}>
+                                            {/* <Text style={styles.nomeProfessor}>
                                             {currentClass.userTeacher.user.firstName} {currentClass.userTeacher.user.lastName.split(" ")[currentClass.userTeacher.user.lastName.length - 1]}
                                         </Text> */}
-                                        <Text style={styles.descAula}>{currentClass.name}</Text>
+                                            <Text style={styles.descAula}>{currentClass.name}</Text>
+                                        </View>
                                     </View>
-                                </View>
-                                <View style={styles.cardDescSala}>
-                                    <Text style={styles.titleAula}>{currentClass.classroom.name} - {currentClass.classroom.floor}° Andar</Text>
-                                    <Text style={styles.descSala}>Começa às {formatTime(currentClass.time)} horas</Text>
-                                </View>
+                                    <View style={styles.cardDescSala}>
+                                        <Text style={styles.titleAula}>{currentClass.classroom.name} - {currentClass.classroom.floor}° Andar</Text>
+                                        <Text style={styles.descSala}>Começa às {formatTime(currentClass.time)} horas</Text>
+                                    </View>
 
 
-                            </TouchableOpacity>
-                        )
-                    })
-                )}
+                                </TouchableOpacity>
+                            )
+                        })
+                    )}
 
-            </ScrollView>
-            <View style={{ width: '100%' }}>
-                <TouchableOpacity style={[
-                    classes.length > 0 ? styles.botaoVerMais1 : styles.botaoVerMais2
-                ]}
-                    onPress={() => navigation.navigate("Calendário")}>
-                    <Text style={styles.textoBotaoVerMais}>Ver mais</Text>
-                </TouchableOpacity>
-            </View>
+                </ScrollView>
+                <View style={{ width: '100%' }}>
+                    <TouchableOpacity style={[
+                        classes.length > 0 ? styles.botaoVerMais1 : styles.botaoVerMais2
+                    ]}
+                        onPress={() => navigation.navigate("Calendário")}>
+                        <Text style={styles.textoBotaoVerMais}>Ver mais</Text>
+                    </TouchableOpacity>
+                </View>
+            </>
+            }
+
         </View>
     )
 }
