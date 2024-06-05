856089cd70028053eea84a9a80d017f8939ff417
GGNOOB1 <geraldovicente987@gmail.com>
2024-06-05 12:44:50 -0300

    Add: Implementando fontes no app inteiro

--- App.tsx
+++ App.tsx
@@ -1,13 +1,27 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppNavigator from '@/navigation/AppNavigator';
 import localeConfigData from '@/utils/ConfigCalendar';
 import { Provider } from 'react-redux';
 import { store } from '@/redux/Store';
+import { useFonts } from '@/utils/useFonts';
 
 localeConfigData;
 
 export default function App() {
+
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const LoadFonts = async () => {
+      await useFonts();
+      setLoading(true)
+    }
+
+    LoadFonts();
+  }, []);
+
+
 
   // const logado = true;
 
@@ -19,10 +33,13 @@ export default function App() {
   //   return alert("Erro no login!")
   // }
 
-  return (
-    <Provider store={store}>
-      <AppNavigator />
-    </Provider>
-  );
+  if (isLoading) {
+    return (
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+    );
+  }
+
 
 }
